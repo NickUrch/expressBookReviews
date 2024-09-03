187051f3a138796d04ea89fa0c45a2d4ed066af1
Nick Urch <nmu0901@yahoo.com>
2024-09-03 14:48:06 -0400

    Updated Task 1

--- final_project/index.js
+++ final_project/index.js
@@ -19,11 +19,11 @@ app.use("/customer/auth/*", function auth(req,res,next){
                 req.user = user;
                 next();
             } else {
-                return res.status(403).json({"message: User not authenticated!"});
+                return res.status(403).json({"message": "User not authenticated!"});
             }
         })
     } else {
-        return res.status(403).json({"message: User not logged in"});
+        return res.status(403).json({"message": "User not logged in"});
     }
 });
  
